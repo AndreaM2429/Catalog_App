c6efea262187fb54da6a2c7442d4a2fced2299e7
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-12 16:03:00 -0600

    Fix linter issues

--- app.rb
+++ app.rb
@@ -5,43 +5,41 @@ class App
 
   def list_all_books
     if @books_list.empty?
-      puts "\n There is no books yet" 
+      puts "\n There is no books yet"
     else
       puts "\n List of books"
-      @books_list.each_with_index { |book, index| puts "#{index}: #{book.label.title} by #{book.autor.first_name} #{book.author.last_name}"}
+      @books_list.each_with_index do |book, index|
+        puts "#{index}: #{book.label.title} by #{book.autor.first_name} #{book.author.last_name}"
+      end
     end
   end
 
-  def add_book
+  def add_a_book
     p "Book's tittle: "
-    label = gets.chomp
-
+    title = gets.chomp
     p "Author's first name: "
     author_name = gets.chomp
-
     p "Author's last name: "
     author_last_name = gets.chomp
-
     p "Book's publish date [dd/mm/yyyy]: "
     publish_date = gets.chomp
-
     p "Book's publisher: "
     publisher = gets.chomp
-
     p "Book's genre: "
     genre = gets.chomp
-
     p "Book's cover color"
     color = gets.chomp
-
     p "Book's cover state: "
     cover_state = gets.chomp
 
+    create_book(publish_date, publisher, cover_state, genre, author_name, author_last_name, title, color)
+  end
+
+  def create_book
     new_book = Book.new(publish_date, publisher, cover_state)
     new_book.genre = Genre.new(genre)
     new_book.author = Author.new(author_name, author_last_name)
     new_book.label = Label.new(title, color)
-
     @books_list << new_book
   end
 end
--- book.rb
+++ book.rb
@@ -2,18 +2,17 @@ require_relative 'item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
+
   def initialize(publish_date, publisher, cover_state, id = nil)
-    super(publish_date, id = id)
+    super(publish_date, id)
     @publisher = publisher
     @cover_state = cover_state
     # @books_list = []
   end
 
   def can_be_archived?
-    if (super || @cover_state == 'bad')
-      return true
-    else
-      false
-    end
+    return true if super || @cover_state == 'bad'
+
+    false
   end
 end
