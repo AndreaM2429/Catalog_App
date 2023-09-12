90415861741d089fc859ae73a90b57a851c84a00
Nicolas Bolañez <Zven94>
2023-09-12 16:29:33 -0300

    add main file with a basic interface for the app

---
+++ main.rb
@@ -0,0 +1,37 @@
+require_relative 'app'
+
+class Main
+  def initialize
+    @app = App.new
+    @menu = {
+      '1' => 'list_all_books', '2' => 'list_all_albums',
+      '3' => 'list_all_movies', '4' => 'list_all_games',
+      '3' => 'list_all_genre', '4' => 'list_all_labels',
+      '5' => 'list_all_authors', '6' => 'add_a_book',
+      '7' => 'add_a_music_album', '8' => 'add_a_game',
+      '9' => 'end_app'
+    }.freeze
+  end
+
+  def menu
+    puts "\nPlease choose an option by enter a number"
+    @menu.each { |key, element| puts "#{key} - #{element.capitalize.gsub '_', ' '}" }
+    print "\nYour option: "
+    action = @menu[gets.chomp]
+    if action
+      action
+    else
+      puts "\n(͠°◡͡°) Invalid option select a valid one\n\n"
+      choose
+    end
+  end
+
+  def start
+    puts "Welcome to the Catalog of my Things App ◦°˚ヽ(•◡•)ノ˚°◦\n\n"
+    loop do
+      @app.send(menu)
+    end
+  end
+end
+
+Main.new.start
